693b98099d85985ee237ae886fa7379b6d27eea8
Kendra-ui <knokam72@gmail.com>
2024-01-03 15:10:26 +0100

    adding files

--- lib/onboarding/onboarding_wrapper.dart
+++ lib/onboarding/onboarding_wrapper.dart
@@ -24,96 +24,92 @@ class _OnboardingWrapperState extends State<OnboardingWrapper> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            if(_currentPage != 0)
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 50,
+          ),
+          if (_currentPage != 0)
             Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => const Splash3()));
-                        },
-                        child: const Text(
-                          'Skip',
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                              color: Color(0xFF24A19C),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        )),
-                  ),
-            Expanded(
-                child: PageView(
-              controller: pageController,
-              onPageChanged: (int page) {
-                setState(() {
-                  _currentPage = page;
-                });
-              },
-              children: _pages,
-            )),
-            const SizedBox(
-              height: 10,
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const Splash3()));
+                  },
+                  child: const Text(
+                    'Skip',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        color: Color(0xFF24A19C),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                  )),
             ),
-            Row(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width/2.3
+          Expanded(
+              child: PageView(
+            controller: pageController,
+            onPageChanged: (int page) {
+              setState(() {
+                _currentPage = page;
+              });
+            },
+            children: _pages,
+          )),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              SizedBox(width: MediaQuery.of(context).size.width / 2.3),
+              SizedBox(
+                height: 6,
+                width: 100,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: _pages.asMap().entries.map((e) {
+                    return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2),
+                        child: Container(
+                          height: 6,
+                          width: _currentPage == e.key ? 24 : 8,
+                          decoration: BoxDecoration(
+                            color: _currentPage == e.key
+                                ? const Color(0xFF24A19C)
+                                : const Color(0xFFCBF1F0),
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                        ));
+                  }).toList(),
                 ),
-                SizedBox(
-                  height: 6,
-                  width: 100,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: _pages.asMap().entries.map((e) {
-                      return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 2),
-                          child: Container(
-                            height: 6,
-                            width: _currentPage == e.key ? 24 : 8,
-                            decoration: BoxDecoration(
-                              color: _currentPage == e.key
-                                  ? const Color(0xFF24A19C)
-                                  : const Color(0xFFCBF1F0),
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                          ));
-                    }).toList(),
-                  ),
-                ),
-              ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 16,
+          ),
+          if (_currentPage != 0)
+            Container(
+              width: MediaQuery.of(context).size.width / 1.5,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(3)),
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          const MaterialStatePropertyAll(Color(0xFF24A19C)),
+                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)))),
+                  onPressed: () {
+                    pageController.nextPage(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.linear);
+                  },
+                  child: const Text(
+                    'Continue',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  )),
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 16,
-            ),
-            if (_currentPage != 0)
-             Container(
-                    width: MediaQuery.of(context).size.width / 1.5,
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(3)),
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: const MaterialStatePropertyAll(
-                                Color(0xFF24A19C)),
-                            shape: MaterialStatePropertyAll(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)))),
-                        onPressed: () {
-                          pageController.nextPage(
-                              duration: const Duration(milliseconds: 500),
-                              curve: Curves.linear);
-                        },
-                        child: const Text(
-                          'Continue',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                        )),
-                  ),
-          ],
-        ),
+        ],
       ),
     );
   }
--- lib/onboarding/splah1.dart
+++ lib/onboarding/splah1.dart
@@ -11,9 +11,8 @@ class _Splash1State extends State<Splash1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          color: Colors.white,
-            child: Column(
+      backgroundColor: Colors.white,
+        body: Column(
       children: [
         SizedBox(
           height: MediaQuery.of(context).size.height / 20,
@@ -21,58 +20,58 @@ class _Splash1State extends State<Splash1> {
       
         Stack(children: <Widget>[
           Center(
-              child: Image.asset(
-            'assets/images/Iphone.png',
-            height: 400,
+          child: Image.asset(
+        'assets/images/Iphone.png',
+        height: 400,
           )),
           Positioned.fill(
-              child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: MediaQuery.of(context).size.height / 5.2,
-              width: MediaQuery.of(context).size.width / 1.5,
-              decoration: const BoxDecoration(boxShadow: [
-                BoxShadow(
-                  color: Colors.white,
-                  blurRadius: 15,
-                  blurStyle: BlurStyle.normal,
-                )
-              ]),
-              child: Column(
-                children: [
-                  const Text(
-                    'Your convenience in making a todo list',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24),
-                  ),
-                  SizedBox(
-                      width: MediaQuery.of(context).size.width / 1.2,
-                      child: const Text(
-                        'Here\'s a mobile platform that helps you create task or to list so that it can help you in every job easier and faster',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.black, fontSize: 14),
-                      )),
-                ],
+          child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: MediaQuery.of(context).size.height / 5.2,
+          width: MediaQuery.of(context).size.width / 1.5,
+          decoration: const BoxDecoration(boxShadow: [
+            BoxShadow(
+              color: Colors.white,
+              blurRadius: 15,
+              blurStyle: BlurStyle.normal,
+            )
+          ]),
+          child: Column(
+            children: [
+              const Text(
+                'Your convenience in making a todo list',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24),
               ),
-            ),
+              SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.2,
+                  child: const Text(
+                    'Here\'s a mobile platform that helps you create task or to list so that it can help you in every job easier and faster',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black, fontSize: 14),
+                  )),
+            ],
+          ),
+        ),
           )),
           Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 30, left: 200.0),
-                child: Image.asset('assets/images/Frame215.png'),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 200.0, bottom: 80),
-                child: Image.asset('assets/images/Frame216.png'),
-              )
-            ],
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 30, left: 200.0),
+            child: Image.asset('assets/images/Frame215.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 200.0, bottom: 80),
+            child: Image.asset('assets/images/Frame216.png'),
+          )
+        ],
           )
         ]),
       ],
-    )));
+    ));
   }
 }
